44896f19574fe0c89aae4cdc9fab4ecaec176b9c
JK <jakub.kazimierski@sensilabs.pl>
2025-03-16 12:18:53 +0100

    zad2 i refactor

---
+++ zajecia1/zad2/matrix.py
@@ -0,0 +1,35 @@
+class Matrix:
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def __add__(self, other):
+        return Matrix(
+            self.a + other.a, self.b + other.b,
+            self.c + other.c, self.d + other.d
+        )
+
+    def __mul__(self, other):
+        return Matrix(
+            self.a * other.a + self.b * other.c, self.a * other.b + self.b * other.d,
+            self.c * other.a + self.d * other.c, self.c * other.b + self.d * other.d
+        )
+
+    def __str__(self):
+        return f"[{self.a}, {self.b};\n {self.c}, {self.d}]"
+
+    def __repr__(self):
+        return f"Matrix({self.a}, {self.b}, {self.c}, {self.d})"
+
+
+# Wywolanie
+m1 = Matrix(1, 2, 3, 4)
+m2 = Matrix(2, 0, 1, 2)
+m3 = m1 + m2
+print(m3)  # [3, 2; 4, 6]
+
+m4 = m1 * m2
+print(m4)  # powinno byc [4, 4; 10, 8]
+print(repr(m4))
